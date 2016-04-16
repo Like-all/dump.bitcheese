70613eb8450ea0cc7f10eedcd14a8bf456c9e592
Voker57 <voker57@gmail.com>
2016-04-17 02:35:58 +0300

    File uploading & accelerated download

---
+++ app/controllers/files_controller.rb
@@ -0,0 +1,30 @@
+require_dependency 'dump'
+
+class FilesController < ApplicationController
+	def upload
+		uploaded = params[:file]
+		if !uploaded.is_a? ActionDispatch::Http::UploadedFile
+			flash[:error] = "This is not a file"
+			redirect_to root_url
+		elsif uploaded.size > Settings.max_file_size
+			flash[:error] = "File is too big!"
+			redirect_to root_url
+		else
+			cleaned_name = Dump.clean_name(uploaded.original_filename)
+			file_key = Dump.gen_suitable_key(Settings.key_size, lambda do |f| !File.exists?(File.join(Settings.dir, "files", f, cleaned_name)) end)
+			file_name = File.join(Settings.dir, "files", file_key, cleaned_name)
+			FileUtils.mkdir_p File.join(Settings.dir, "files", file_key)
+			File.open(file_name, "wb") do |f| f.write uploaded.read end
+			redirect_to url_for(controller: "files", action: "preview", slug: file_key, filename: cleaned_name)
+		end
+	end
+	
+	def preview
+		slug = params[:slug]
+		filename = params[:filename]
+	end
+	
+	def download
+		send_file "#{Settings.dir}/files/#{params[:slug]}/#{params[:filename]}", x_sendfile: true
+	end
+end
--- lib/dump.rb
+++ lib/dump.rb
@@ -1,9 +1,26 @@
 class Dump
-	def max_file_size
-		20.megabytes
+	def self.gen_key(len)
+		unvowels = "wrtpsdfghjklzxcvbnm".chars
+		vowels = "eyuioa".chars
+		key = ""
+		len.times do |i|
+			if i % 2 == 0
+				key << vowels.sample
+			else
+				key << unvowels.sample
+			end
+		end
+		key
 	end
 	
-	def max_image_size
-		10.megabytes
+	def self.gen_suitable_key(len, tst)
+		begin
+			key = self.gen_key(len)
+		end until tst.call(key)
+		key
+	end
+	
+	def self.clean_name(name)
+		name.gsub(/[\s\0\/]/, "_")
 	end
 end
