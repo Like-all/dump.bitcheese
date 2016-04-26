1f126233c49be9845cdbfbbb2fea59ba71e4600b
Voker57 <voker57@gmail.com>
2016-04-26 14:28:01 +0300

    puma restart fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, "dump"
 set :rvm_ruby_string, `cat .ruby-version`
 set :linked_files, %w(config/database.yml config/secrets.yml config/environments/production.rb config/settings/production.yml)
-set :linked_dirs, %w(log tmp/pids)
+set :linked_dirs, %w(log tmp/pids tmp/sockets)
 
 after 'deploy:publishing', 'puma:restart'
