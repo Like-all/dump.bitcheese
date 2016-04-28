99a193230514c921dcf5b19dad7b9ea526baa23b
Voker57 <voker57@gmail.com>
2016-04-28 15:23:05 +0300

    forgotten job file

---
+++ app/jobs/reaping_obscure_files_job.rb
@@ -0,0 +1,10 @@
+class ReapingObscureFilesJob < ActiveJob::Base
+	queue_as :default
+
+	def perform
+		while DumpedFile.where(file_frozen: false).sum(:size) > Settings.storage_limit
+			victim = DumpedFile.order("accessed_at ASC").first(5).sample
+			victim.glaciate!
+		end
+	end
+end
