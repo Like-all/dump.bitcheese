26f1e67f852bedcf45c6865df7cc007e3b5a3832
Voker57 <voker57@gmail.com>
2016-06-19 21:00:44 +0300

    never accessed files in stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,6 +3,7 @@ class StatsController < ApplicationController
 
 	def index
 		@obscure_files = DumpedFile.where("file_frozen = false AND accessed_at < ?", DateTime.now - 1.month)
+		@very_obscure_files = DumpedFile.where("file_frozen = false AND accessed_at = (SELECT min(accessed_at) FROM dumped_files)")
 		@frozen_files = DumpedFile.where(file_frozen: true)
 		@live_files = DumpedFile.where(file_frozen: false)
 		@uploads_last_day = Upload.where("created_at > ?", DateTime.now - 1.day)
